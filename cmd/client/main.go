bb0a0f97f384f0cf97e5dda0de2a2791b1ffb516
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2023-05-28 10:29:49 -0300

    Added comments explaining Client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -12,28 +12,35 @@ func main() {
 }
 
 func Ping() {
+	// Get information about the RPC server running in a given address
 	tSocket := thrift.NewTSocketConf("localhost:8080", nil)
 	transportFactory := thrift.NewTTransportFactory()
 
+	// Fetch the transport from the socket fetched above
 	transport, err := transportFactory.GetTransport(tSocket)
 	if err != nil {
 		return
 	}
 	defer transport.Close()
 
+	// Opens the transport for communication
 	if err = transport.Open(); err != nil {
 		log.Println("failed to ping with error:", err)
 		return
 	}
 
+	// Initialize the protocol factory of type CompactProtocol.
+	// More info about protocols here: https://thrift-tutorial.readthedocs.io/en/latest/thrift-stack.html
 	protocolFactory := thrift.NewTCompactProtocolFactoryConf(&thrift.TConfiguration{})
+
+	// Creates the RPC client using the transport + protocol factory
 	cli := myapp.NewUserServiceClientFactory(transport, protocolFactory)
 
+	// Calls RPC Server
 	resp, err := cli.Ping(context.Background())
 	if err != nil {
 		log.Println("failed to ping with error:", err)
 		return
 	}
-
 	log.Printf("Success: \nMessage:%s \nVersion:%d", resp.GetMessage(), resp.GetVersion())
 }
